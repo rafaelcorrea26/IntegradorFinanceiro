91492c36630bbe4983b0f27daaed328be6a15967
Rafael Brum Corrêa <54960396+rafaelcorrea26@users.noreply.github.com>
2024-06-17 10:02:30 -0300

    Update uCidadeDAO.pas

--- Model/DAO/uCidadeDAO.pas
+++ Model/DAO/uCidadeDAO.pas
@@ -23,12 +23,12 @@ type
 
   {
 
-    CREATE TABLE MC300_CID_SEFRAZ (
-    AC300_CODIGO        VARCHAR(14) NOT NULL,
-    AC300_NOME          NOME /* NOME = VARCHAR(35) */,
-    AC300_UF            UF /* UF = CHAR(2) */,
-    AC300_CEP           CEP /* CEP = VARCHAR(10) */,
-    AN300_ALIQUOTA_ISS  DINHEIRO /* DINHEIRO = NUMERIC(15,4) DEFAULT 0 */
+    CREATE TABLE CIDSEFAZ (
+    CODIGO        VARCHAR(14) NOT NULL,
+    NOME          NOME /* NOME = VARCHAR(35) */,
+    UF            UF /* UF = CHAR(2) */,
+    CEP           CEP /* CEP = VARCHAR(10) */,
+    ALIQUOTA_ISS  DINHEIRO /* DINHEIRO = NUMERIC(15,4) DEFAULT 0 */
     );
 
   }
@@ -44,8 +44,8 @@ begin
 
   lQuery := TQuery.Create(nil);
   try
-    lQuery.SQL.Add('SELECT * FROM MC300_CID_SEFRAZ WHERE AC300_CODIGO = :AC300_CODIGO');
-    lQuery.ParamByName('AC300_CODIGO').AsString := pCidade.Codigo;
+    lQuery.SQL.Add('SELECT * FROM CIDSEFAZ WHERE CODIGO = :CODIGO');
+    lQuery.ParamByName('CODIGO').AsString := pCidade.Codigo;
     lQuery.Open;
 
     if (lQuery.RecordCount > 0) then
@@ -67,17 +67,17 @@ begin
 
     lQuery.Close;
     lQuery.SQL.Clear;
-    lQuery.SQL.Add('UPDATE MC300_CID_SEFRAZ SET                  ');
-    lQuery.SQL.Add('     AC300_NOME         =:AC300_NOME         ');
-    lQuery.SQL.Add('   , AC300_UF           =:AC300_UF           ');
-    lQuery.SQL.Add('   , AC300_CEP          =:AC300_CEP          ');
-    lQuery.SQL.Add('   , AN300_ALIQUOTA_ISS =:AN300_ALIQUOTA_ISS ');
-    lQuery.SQL.Add('WHERE AC300_CODIGO      =:AC300_CODIGO       ');
-    lQuery.ParamByName('AC300_NOME').AsString := copy(pCidade.Nome,1,35);
-    lQuery.ParamByName('AC300_UF').AsString := copy(pCidade.Uf,1,2);
-    lQuery.ParamByName('AC300_CEP').AsString := copy(pCidade.Cep,1,10);
-    lQuery.ParamByName('AN300_ALIQUOTA_ISS').AsFloat := pCidade.Aliquota;
-    lQuery.ParamByName('AC300_CODIGO').AsString := copy(pCidade.Codigo,1,14);
+    lQuery.SQL.Add('UPDATE CIDSEFAZ SET                  ');
+    lQuery.SQL.Add('     NOME         =:NOME         ');
+    lQuery.SQL.Add('   , UF           =:UF           ');
+    lQuery.SQL.Add('   , CEP          =:CEP          ');
+    lQuery.SQL.Add('   , ALIQUOTA_ISS =:ALIQUOTA_ISS ');
+    lQuery.SQL.Add('WHERE CODIGO      =:CODIGO       ');
+    lQuery.ParamByName('NOME').AsString := copy(pCidade.Nome,1,35);
+    lQuery.ParamByName('UF').AsString := copy(pCidade.Uf,1,2);
+    lQuery.ParamByName('CEP').AsString := copy(pCidade.Cep,1,10);
+    lQuery.ParamByName('ALIQUOTA_ISS').AsFloat := pCidade.Aliquota;
+    lQuery.ParamByName('CODIGO').AsString := copy(pCidade.Codigo,1,14);
     lQuery.ExecSQL;
     lQuery.Connection.Commit;
   finally
@@ -93,14 +93,14 @@ begin
   try
     lQuery.Close;
     lQuery.SQL.Clear;
-    lQuery.SQL.Add('select * from MC300_CID_SEFRAZ where AC300_CODIGO = :AC300_CODIGO ');
-    lQuery.ParamByName('AC300_CODIGO').AsString := pCidade.Codigo;
+    lQuery.SQL.Add('select * from CIDSEFAZ where CODIGO = :CODIGO ');
+    lQuery.ParamByName('CODIGO').AsString := pCidade.Codigo;
     lQuery.Open;
 
-    pCidade.Nome := lQuery.FieldByName('AC300_NOME').AsString;
-    pCidade.Uf := lQuery.FieldByName('AC300_UF').AsString;
-    pCidade.Cep := lQuery.FieldByName('AC300_CEP').AsString;
-    pCidade.Aliquota := lQuery.FieldByName('AN300_ALIQUOTA_ISS').AsFloat;
+    pCidade.Nome := lQuery.FieldByName('NOME').AsString;
+    pCidade.Uf := lQuery.FieldByName('UF').AsString;
+    pCidade.Cep := lQuery.FieldByName('CEP').AsString;
+    pCidade.Aliquota := lQuery.FieldByName('ALIQUOTA_ISS').AsFloat;
   finally
     lQuery.Free;
   end;
@@ -114,9 +114,9 @@ begin
   try
     lQuery.Close;
     lQuery.SQL.Clear;
-    lQuery.SQL.Add('DELETE FROM MC300_CID_SEFRAZ       ');
-    lQuery.SQL.Add('WHERE AC300_CODIGO =:AC300_CODIGO  ');
-    lQuery.ParamByName('AC300_CODIGO').AsString := pCodigo;
+    lQuery.SQL.Add('DELETE FROM CIDSEFAZ       ');
+    lQuery.SQL.Add('WHERE CODIGO =:CODIGO  ');
+    lQuery.ParamByName('CODIGO').AsString := pCodigo;
     lQuery.ExecSQL;
 
     lQuery.Connection.Commit;
@@ -133,24 +133,24 @@ begin
   try
     lQuery.Close;
     lQuery.SQL.Clear;
-    lQuery.SQL.Add('INSERT INTO MC300_CID_SEFRAZ( ');
-    lQuery.SQL.Add('     AC300_CODIGO             ');
-    lQuery.SQL.Add('   , AC300_NOME               ');
-    lQuery.SQL.Add('   , AC300_UF                 ');
-    lQuery.SQL.Add('   , AC300_CEP                ');
-    lQuery.SQL.Add('   , AN300_ALIQUOTA_ISS       ');
+    lQuery.SQL.Add('INSERT INTO CIDSEFAZ( ');
+    lQuery.SQL.Add('     CODIGO             ');
+    lQuery.SQL.Add('   , NOME               ');
+    lQuery.SQL.Add('   , UF                 ');
+    lQuery.SQL.Add('   , CEP                ');
+    lQuery.SQL.Add('   , ALIQUOTA_ISS       ');
     lQuery.SQL.Add('   ) VALUES (                 ');
-    lQuery.SQL.Add('     :AC300_CODIGO            ');
-    lQuery.SQL.Add('   , :AC300_NOME              ');
-    lQuery.SQL.Add('   , :AC300_UF                ');
-    lQuery.SQL.Add('   , :AC300_CEP               ');
-    lQuery.SQL.Add('   , :AN300_ALIQUOTA_ISS      ');
+    lQuery.SQL.Add('     :CODIGO            ');
+    lQuery.SQL.Add('   , :NOME              ');
+    lQuery.SQL.Add('   , :UF                ');
+    lQuery.SQL.Add('   , :CEP               ');
+    lQuery.SQL.Add('   , :ALIQUOTA_ISS      ');
     lQuery.SQL.Add('   )                              ');
-    lQuery.ParamByName('AC300_NOME').AsString := copy(pCidade.Nome,1,35);
-    lQuery.ParamByName('AC300_UF').AsString := copy(pCidade.Uf,1,2);
-    lQuery.ParamByName('AC300_CEP').AsString := copy(pCidade.Cep,1,10);
-    lQuery.ParamByName('AN300_ALIQUOTA_ISS').AsFloat := pCidade.Aliquota;
-    lQuery.ParamByName('AC300_CODIGO').AsString := copy(pCidade.Codigo,1,14);
+    lQuery.ParamByName('NOME').AsString := copy(pCidade.Nome,1,35);
+    lQuery.ParamByName('UF').AsString := copy(pCidade.Uf,1,2);
+    lQuery.ParamByName('CEP').AsString := copy(pCidade.Cep,1,10);
+    lQuery.ParamByName('ALIQUOTA_ISS').AsFloat := pCidade.Aliquota;
+    lQuery.ParamByName('CODIGO').AsString := copy(pCidade.Codigo,1,14);
 
     lQuery.ExecSQL;
     lQuery.Connection.Commit;
@@ -163,5 +163,4 @@ class function TCidadeDAO.Limpar: boolean;
 begin
 
 end;
-
 end.
